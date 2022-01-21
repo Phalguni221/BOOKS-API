e92d679ef8e247f744aac50392cdc168b94be248
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-01-21 13:02:26 -0500

    Add files via upload

--- books.js
+++ books.js
@@ -1,13 +1,127 @@
-const mongoose = require('mongoose')
+const router = require('express').Router()
+const db = require('../Models/books')
 
-const bookSchema = new mongoose.Schema({
-    title: {type:String, default:'Anonymous'},
-    description: {type:String, default:'N/A'},
-    year: {type:Number},
-    quantity: {type:Number},
-    imageURL: {type:String, default:'https://placekitten.com/200/300'}
+// GET /books/seed route
+router.get('/books/seed', (req, res) => {[(
+    {
+        title : "The Shinobi Initiative",
+        description : "The reality-bending adventures of a clandestine service agency in the year 2166",
+        year : 2014,
+        quantity : 10,
+        imageURL : "https://imgur.com/LEqsHy5.jpeg"
+    },
+
+    {
+        "title": "Tess the Wonder Dog",
+        "description": "The tale of a dog who gets super powers",
+        "year": 2007,
+        "quantity": 3,
+        "imageURL": "https://imgur.com/cEJmGKV.jpg"
+    },
+
+    {
+        "title": "The Annals of Arathrae",
+        "description": "This anthology tells the intertwined narratives of six fairy tales.",
+        "year": 2016,
+        "quantity": 8,
+        "imageURL": "https://imgur.com/VGyUtrr.jpeg"
+    },
+
+    {
+        "title": "W∀RP",
+        "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+        "year": 2010,
+        "quantity": 4,
+        "imageURL": "https://imgur.com/qYLKtPH.jpeg"
+    }
+)]}
+)
+    //   .then(books => {
+    //     res.json(books)
+    // }
+    // )
+   .catch(err => {
+       console.log(err)
+       res.render('error404')
+   })
+
+//GET /books route
+router.get('/books', (req,res) => {
+    db.Book.find()
+    .then(Books => {
+        res.json(Books)
+    }
+    )
+   .catch(err => {
+       console.log(err)
+       res.render('error404')
+   })
 })
-  
-  bookSchema.methods.showEstablished = function() {}
-    
-  module.exports = mongoose.model('Book', bookSchema)
+
+ // GET /books/:id route
+router.get('/books/:id', (req,res) => {
+    db.Book.find()
+    .then(books => {
+        res.json(books)
+    }
+    )
+   .catch(err => {
+       console.log(err)
+       res.send('error404')
+   })
+})
+
+//PUT route for /books/:id
+router.put('/books/:id', (req,res) => {
+    db.Book.find()
+    .then(books => {
+        res.json(books)
+    }
+    )
+   .catch(err => {
+       console.log(err)
+       res.send('error404')
+   })
+})
+
+//PATCH route /books/:id
+router.put('/books/:id', (req,res) => {
+    db.Book.find()
+    .then(books => {
+        res.json(books)
+    }
+    )
+   .catch(err => {
+       console.log(err)
+       res.send('error404')
+   })
+})
+
+//DELETE route for /books/:id
+router.delete('/books/:id', (req,res) => {
+    db.Book.find()
+    .then(books => {
+        res.json(books)
+    }
+    )
+   .catch(err => {
+       console.log(err)
+       res.send('error404')
+   })
+})
+
+
+//POST router for /books/:id
+router.post('/books/:id', (req,res) => {
+    db.Book.create()
+    .then(books => {
+        res.json(books)
+    }
+    )
+   .catch(err => {
+       console.log(err)
+       res.send('error404')
+   })
+})
+
+module.exports = router
