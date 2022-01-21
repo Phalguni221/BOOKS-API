d28f795253589ed3660105b2ae82d813064a09f4
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-01-21 12:56:30 -0500

    Add files via upload

--- Models/books.js
+++ Models/books.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
-let bookSchema = new mongoose.Schema({
+const bookSchema = new mongoose.Schema({
     title: {type:String, default:'Anonymous'},
     description: {type:String, default:'N/A'},
     year: {type:Number},
     quantity: {type:Number},
-    imageURL: {type:String, default:'https://placekitten.com/200/300'},
+    imageURL: {type:String, default:'https://placekitten.com/200/300'}
 })
   
   bookSchema.methods.showEstablished = function() {}
