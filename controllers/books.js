3c538a2413b08217a7b62895f5e4e112dfc29365
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-01-21 13:02:43 -0500

    Add files via upload

--- controllers/books.js
+++ controllers/books.js
@@ -2,47 +2,48 @@ const router = require('express').Router()
 const db = require('../Models/books')
 
 // GET /books/seed route
-// router.get('/books/seed', (req, res) => {
-//     {
-//         title : "The Shinobi Initiative",
-//         description : "The reality-bending adventures of a clandestine service agency in the year 2166",
-//         year : 2014,
-//         quantity : 10,
-//         imageURL : "https://imgur.com/LEqsHy5.jpeg"
-//     },
+router.get('/books/seed', (req, res) => {[(
+    {
+        title : "The Shinobi Initiative",
+        description : "The reality-bending adventures of a clandestine service agency in the year 2166",
+        year : 2014,
+        quantity : 10,
+        imageURL : "https://imgur.com/LEqsHy5.jpeg"
+    },
 
-//     {
-//         "title": "Tess the Wonder Dog",
-//         "description": "The tale of a dog who gets super powers",
-//         "year": 2007,
-//         "quantity": 3,
-//         "imageURL": "https://imgur.com/cEJmGKV.jpg"
-//     },
+    {
+        "title": "Tess the Wonder Dog",
+        "description": "The tale of a dog who gets super powers",
+        "year": 2007,
+        "quantity": 3,
+        "imageURL": "https://imgur.com/cEJmGKV.jpg"
+    },
 
-//     {
-//         "title": "The Annals of Arathrae",
-//         "description": "This anthology tells the intertwined narratives of six fairy tales.",
-//         "year": 2016,
-//         "quantity": 8,
-//         "imageURL": "https://imgur.com/VGyUtrr.jpeg"
-//     },
+    {
+        "title": "The Annals of Arathrae",
+        "description": "This anthology tells the intertwined narratives of six fairy tales.",
+        "year": 2016,
+        "quantity": 8,
+        "imageURL": "https://imgur.com/VGyUtrr.jpeg"
+    },
 
-//     {
-//         "title": "W∀RP",
-//         "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
-//         "year": 2010,
-//         "quantity": 4,
-//         "imageURL": "https://imgur.com/qYLKtPH.jpeg"
-//     }
-// })
-//     //   .then(books => {
-//     //     res.json(books)
-//     // }
-//     // )
-//    .catch(err => {
-//        console.log(err)
-//        res.render('error404')
-//    })
+    {
+        "title": "W∀RP",
+        "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+        "year": 2010,
+        "quantity": 4,
+        "imageURL": "https://imgur.com/qYLKtPH.jpeg"
+    }
+)]}
+)
+    //   .then(books => {
+    //     res.json(books)
+    // }
+    // )
+   .catch(err => {
+       console.log(err)
+       res.render('error404')
+   })
 
 //GET /books route
 router.get('/books', (req,res) => {
